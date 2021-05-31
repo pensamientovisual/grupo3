b589b05410fd8a59a16adbfdf00d67e529e5eb4a
Maria Paz Figueroa <mariapazfigueroa@MacBook-Air-de-Paci.local>
2021-05-30 20:11:01 -0400

    domingo

--- visual.js
+++ visual.js
@@ -11,31 +11,6 @@ $(document).ready(function(){
     $("#nombrecomuna").text("COMUNA");
     });
 
-    var boton1 = $(".cerrillos")
-    var boton2 = $(".cerronavia")
-    var boton3 = $(".conchali")
-    var boton4 = $(".elbosque")
-    var boton5 = $(".estacioncentral")
-    var boton6 = $(".huechuraba")
-    var limpiar = $("#boton")
-    
-    boton1.click(function(){
-      $("#gcerrillos").css("display","block");
-    
-    });
-
-    boton2.click(function(){
-      $("#gcerronavia").css("display","block");
-    });
-
-
-    limpiar.click(function(){
-      $("#gcerrillos").css("display","none")
-      $("#gcerronavia").css("display","none")
-    });
-
-
-
 
 
  
